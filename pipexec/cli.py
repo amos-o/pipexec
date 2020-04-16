f2114b1a2192a9b7d5b47f441518fd5a945e5e70
Amos Omondi <amos.omondi@andela.com>
2020-04-17 02:00:15 +0300

    Handle CTRL+C

--- pipexec/cli.py
+++ pipexec/cli.py
@@ -6,17 +6,34 @@ import typer
 app = typer.Typer()
 
 
-@app.command()
-def pipexec(package: str):
-    typer.echo(f'Starting your {package} shell...')
+def setup(package):
+    """
+    Setup the environment.
+    """
+    typer.secho(f'Starting your {package} shell...', fg=typer.colors.GREEN)
     subprocess.call(['python3', '-m', 'venv', '.venv'])
     subprocess.call(['/bin/sh', '.venv/bin/activate'])
     subprocess.call(['pip', 'install', f'{package}'])
-    subprocess.call(['python3'])
-    typer.echo('Cleaning up...')
+
+
+def teardown():
+    """
+    Remove the environment.
+    """
+    typer.secho('\nCleaning up...', fg=typer.colors.GREEN)
     subprocess.call(['rm', '-rf', '.venv'])
-    typer.echo('Done.')
-    typer.echo('Bye!')
+    typer.secho('Done', fg=typer.colors.GREEN)
+
+
+@app.command()
+def pipexec(package: str):
+    setup(package)
+    try:
+        subprocess.call(['python3'])
+        teardown()
+    except KeyboardInterrupt:
+        # handle exit from shell using CTRL + C
+        teardown()
 
 
 def main():
